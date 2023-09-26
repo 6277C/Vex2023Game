63bcb991e23b8ca29751d0f0ea30569bab927a21
6277C <145620800+6277C@users.noreply.github.com>
2023-09-26 13:20:43 -0400

    Add files via upload

---
+++ include/robot-config.h
@@ -0,0 +1,23 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern inertial Inertial10;
+extern motor FR;
+extern motor MR;
+extern motor BR;
+extern motor FL;
+extern motor ML;
+extern motor BL;
+extern digital_out DigitalOutA;
+extern controller Controller1;
+extern motor catapult;
+extern rotation catapultRot;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
---
+++ src/robot-config.cpp
@@ -0,0 +1,34 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+inertial Inertial10 = inertial(PORT10);
+motor FR = motor(PORT11, ratio6_1, false);
+motor MR = motor(PORT12, ratio6_1, false);
+motor BR = motor(PORT13, ratio6_1, false);
+motor FL = motor(PORT20, ratio6_1, true);
+motor ML = motor(PORT19, ratio6_1, true);
+motor BL = motor(PORT18, ratio6_1, true);
+digital_out DigitalOutA = digital_out(Brain.ThreeWirePort.A);
+controller Controller1 = controller(primary);
+motor catapult = motor(PORT8, ratio36_1, false);
+rotation catapultRot = rotation(PORT2, false);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
