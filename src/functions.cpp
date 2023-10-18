60023f7fa33b68686d3075809f264c2973322ac6
6277C <dey_ryan@harveyschool.org>
2023-10-18 15:09:24 -0400

    formatting

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,6 +1,6 @@
 #include "vex.h"
 
-//make bool true for plows to go out and make bool false for plows to pull in
+// make bool true for plows to go out and make bool false for plows to pull in
 void plowVoid(bool trans)
 {
     if (trans == true)
@@ -22,27 +22,26 @@ void catapultVoid()
 
 void drawGUI()
 {
-Brain.Screen.clearScreen();
-Brain.Screen.setFillColor(red);
-Brain.Screen.drawRectangle(0,0,240,240);
-Brain.Screen.setFillColor(blue);
-Brain.Screen.drawRectangle(240,0,240,240);
-Brain.Screen.setFont(monoXXL);
-Brain.Screen.printAt(120,60, "Blake");
-Brain.Screen.printAt(360, 60, "Meaghan");
-
+    Brain.Screen.clearScreen();
+    Brain.Screen.setFillColor(red);
+    Brain.Screen.drawRectangle(0, 0, 240, 240);
+    Brain.Screen.setFillColor(blue);
+    Brain.Screen.drawRectangle(240, 0, 240, 240);
+    Brain.Screen.setFont(monoXXL);
+    Brain.Screen.printAt(120, 60, "Blake");
+    Brain.Screen.printAt(360, 60, "Meaghan");
 }
 
 void driverSelector()
 {
     int x = Brain.Screen.xPosition();
 
-  if (x <= 240)
-  {
-    driver = 1;
-  }
-  else if (x >= 240)
-  {
-    driver = 2;
-  }
+    if (x <= 240)
+    {
+        driver = 1;
+    }
+    else if (x >= 240)
+    {
+        driver = 2;
+    }
 }
