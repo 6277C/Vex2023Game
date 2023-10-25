6fb471cbd705a60a6e118e0757e8e4d6faf52ea5
6277C <dey_ryan@harveyschool.org>
2023-10-25 09:49:21 -0400

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -184,7 +184,7 @@ void autonomous(void)
 
 void usercontrol(void)
 {
- auto_started = true;
+  auto_started = true;
   FR.setStopping(coast);
   MR.setStopping(coast);
   BR.setStopping(coast);
@@ -194,7 +194,7 @@ void usercontrol(void)
   if (driver == 1)
   {
     Brain.Screen.setFillColor(blue);
-    Brain.Screen.drawRectangle(0,0,480,240);
+    Brain.Screen.drawRectangle(0, 0, 480, 240);
     while (1)
     {
       // Catapult Prime
@@ -340,7 +340,7 @@ void usercontrol(void)
   else if (driver == 2)
   {
     Brain.Screen.setFillColor("#FFC0CB");
-    Brain.Screen.drawRectangle(0,0,480,240);
+    Brain.Screen.drawRectangle(0, 0, 480, 240);
     while (1)
     {
       // Catapult Prime
